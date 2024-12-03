3f8d424512b59b1f742c43a33f540d64fe53ccc9
Parthsukhadiya18 <121274802+Parthsukhadiya18@users.noreply.github.com>
2024-12-03 11:37:47 +0530

    Add files via upload

---
+++ js/exjs.js
@@ -0,0 +1,6 @@
+document.write('Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, alias architecto accusamus hic velit aperiam totam quas dolores dignissimos doloribus excepturi id laboriosam illo corporis repudiandae sed aliquam quia expedita, explicabo vero! Esse consectetur fuga ea. Minus ea eum ut esse laudantium, nam vitae perspiciatis sunt labore laboriosam veniam velit cupiditate explicabo totam libero ullam doloribus dolorem commodi. Magni quo mollitia rem quis repudiandae hic vitae tenetur ipsa ducimus eaque? Vero architecto minus laudantium, dolorum, sed quo earum unde exercitationem est, eveniet magni beatae! Eligendi vel non, repudiandae perferendis totam eos reiciendis beatae fugit nostrum debitis. Animi accusantium incidunt libero?')
+
+
+
+document.write('<h1>milap</h1>')
+document.write('<b>milap</b>')
