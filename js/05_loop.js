8e1148dbf1885256968779ce2ac0a67156966122
Parthsukhadiya18 <121274802+Parthsukhadiya18@users.noreply.github.com>
2024-12-06 12:03:24 +0530

    Add files via upload

---
+++ js/05_loop.js
@@ -0,0 +1,68 @@
+// let num1 = 10
+// while (num1 < 12) {
+//     num1++;
+//     console.log(num1 + " ");
+
+// }
+
+
+// do {
+//     num1++;
+//     console.log(num1 + " ");
+
+// } while (num1 < 12);
+
+// for (let i = 0; i < 10; i++) {
+
+//     console.log(i);
+
+
+// }
+
+// console.log("+");
+
+// let num2=[1,2,3,4,5,6];
+
+// let text="";
+
+// for (let i = 0; i <num2.length; i++) {
+
+
+//     text += num2[i];
+
+// }
+// console.log(text);
+
+
+// let info = { name: "parth", City: "Surat" }
+// let text = "";
+// for (const X in info) {
+//     text += info[X]+" ";
+// }
+// console.log(text);
+
+// let num3=[10,2,49,37,1,92]
+// let text=""
+// for (const X of num3) {
+//     text+= X+" ";
+// }
+// console.log(text);
+
+// let num3=[10,2,49,37,1,92]
+// // let text=""
+
+// num3.forEach(x => {
+//     text+=[x];
+// });
+// console.log(text);
+
+
+let info = "parthpatelsurattrueline"
+let text = " ";
+ for (const x of info) {
+    text+=x+" ";  
+ }
+console.log(text);
+
+
+
---
+++ js/05_vartype.js
@@ -0,0 +1,51 @@
+
+// string 
+let name = "parth";
+console.log("name",name);
+// number 
+let num1= 100;
+console.log(num1);
+// null 
+let city=""
+console.log(city);
+// undefined 
+let salary;
+console.log(salary);
+
+
+// array 
+let area=["Surat","Vesu","Althan"];
+console.log(area);
+
+let info1=["Surat","",,1];
+console.log(info1);
+
+
+// obj 
+let info={
+    name:"Parth",
+    City:"Pune",
+    salary:"",
+adress:{
+    BNo:1,
+    S_Name:"SUrat",
+    Pincode:1008
+
+}
+}
+console.log(info);
+console.log(info.name);
+console.log(info.city);
+console.log(info.adress);
+console.log(info.adress.BNo);
+
+
+
+
+
+
+
+
+
+
+
