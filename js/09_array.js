acd9eb611552918ef35e4778a2a59bc7529b4f9a
Parthsukhadiya18 <121274802+Parthsukhadiya18@users.noreply.github.com>
2024-12-11 12:39:01 +0530

    Add files via upload

---
+++ js/09_array.js
@@ -0,0 +1,124 @@
+// let num1=[1,2,3,6,5,4];
+// num1.sort()
+// console.log("sort",num1.sort());
+
+// num1.reverse()
+// console.log("sort",num1.reverse());
+
+// num1.pop()  last delete value 
+// console.log("sort",num1.pop());
+// console.log("sort",num1);
+
+// push  last add value 
+
+// console.log("sort",num1.push(100));
+// console.log("sort",num1);
+
+// shift () first value delete 
+// console.log("sort", num1.shift());
+// console.log("sort", num1);
+
+// // unshif () fist add value 
+// console.log("sort", num1.unshift(100));
+// console.log("sort", num1);
+// 
+// console.log("sort", num1.length);
+// let num1 = [1, 2, 3,56,7,8,9]
+// let num2 = [6, 5, 4];
+// // concat ()
+// console.log("concat", num1.concat(num2));
+
+// console.log("concat", num1);
+// join () 
+// console.log("join", num1.join("*"));
+
+// console.log("join", num1);
+// // slice () 
+
+// console.log("slice", num1.slice(0,5));
+
+
+// console.log("slice", num1);
+// console.log("isArray",Array.isArray(num1));
+
+
+
+// let a = [1, 2, 3, 4];
+// console.log(a.slice(1));
+// console.log(a.slice(1, 3));
+
+
+// let a="1,2,43,4,5,6";
+// console.log(Array.isArray(a));
+
+// let text = "parth patel surat pune patel";
+// console.log("indexof ",text.indexOf("parth"));
+// console.log("lastIndexOf ",text.lastIndexOf("patel"));
+// console.log("lastIndexOf ",text.includes("patal"));
+
+
+
+// a = ["parth","nikunj","jeet"];
+// // console.log("splice ", a.splice(1)); a.splice(1)
+// // array (start, delete, add) 
+// console.log("splice ", a.splice(1));
+// console.log("splice ", a);
+
+// age = [0, 12, 13, 14, 17];
+
+// console.log(age.some(chak));
+
+
+// function chak(age) {
+//     return age >= 10
+// }
+
+// console.log(age.every(chak));
+// // console.log("filter",age.filter(chak));
+
+
+
+// let text = ["parth","jeet"]
+// console.log(text.toString());
+let num1 = [1,2,3,4]
+
+// console.log(num1.fill("100"));
+// console.log(num1.fill("100",2));
+// console.log(num1.fill("100",1,3));
+console.log(num1.fill("100",1,2));
+
+
+
+
+
+
+// console.log(text.forEach((element) => {
+//     return element;
+// }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
